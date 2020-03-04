6f931f6b9dca421bcf10e7e8f7b35bb551a20f7d
rayshen <rayshen@redpay.io>
2020-03-04 10:24:53 +0800

    First commit

---
+++ app/src/main/java/design/ws/com/wallet/Fragment_First.java
@@ -0,0 +1,27 @@
+package design.ws.com.wallet;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rp on 8/30/2016.
+ */
+public class Fragment_First extends Fragment {
+
+    private View view;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_first, container, false);
+
+
+
+        return view;
+
+    }
+    }
